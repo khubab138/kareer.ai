73b25a766be654b0fa7360a766936b8128350cce
Khubab138 <muhammadkhubabsiddiqui@gmail.com>
2025-11-22 03:28:06 +0500

    Completed Mock interview section, generating question by ai and updating a score question and result to db

--- actions/interview.js
+++ actions/interview.js
@@ -2,6 +2,14 @@
 
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY, {
+  apiVersion: "v1",
+});
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.5-flash",
+});
 
 export async function generateQuiz() {
   const { userId } = await auth();
@@ -14,12 +22,13 @@ export async function generateQuiz() {
 
   if (!user) throw new Error("User Not Found");
 
-  const prompt = `
+  try {
+    const prompt = `
     Generate 10 technical interview questions for a ${
       user.industry
     } professional${
-    user.skills?.length ? ` with expertise in ${user.skills.join(", ")}` : ""
-  }.
+      user.skills?.length ? ` with expertise in ${user.skills.join(", ")}` : ""
+    }.
     
     Each question should be multiple choice with 4 options.
     
@@ -36,7 +45,82 @@ export async function generateQuiz() {
     }
   `;
 
-  const result = await model.generateContent(prompt);
-  const response = result.response;
-  const text = response.text();
+    const result = await model.generateContent(prompt);
+    const response = result.response;
+    const text = response.text();
+
+    const cleanedText = text.replace(/```(?:json)?\n?/g, "").trim();
+    const quiz = JSON.parse(cleanedText);
+
+    return quiz.questions;
+  } catch (error) {
+    console.error("Error generating quiz:", error);
+    throw new Error("Failed to generate quiz questions");
+  }
+}
+
+export async function saveQuizResult(questions, answers, score) {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+  const user = await db.user.findUnique({
+    where: {
+      clerkUserId: userId,
+    },
+  });
+
+  if (!user) throw new Error("User Not Found");
+
+  const questionResults = questions.map((q, i) => ({
+    question: q.question,
+    answer: q.answer,
+    userAnswer: answers[i],
+    isCorrect: q.correctAnswer === answers[i],
+    explanation: q.explanation,
+  }));
+
+  const wrongAnswers = questionResults.filter((q) => !q.isCorrect);
+  let improvementTip = null;
+
+  if (wrongAnswers.length > 0) {
+    const wrongQuestionsText = wrongAnswers
+      .map(
+        (q) =>
+          `Question:"${q.question}"\nCorrect Answer: "${q.answer}"\nUser Answer: "${q.userAnswer}`
+      )
+      .join("\n\n");
+
+    const improvementPrompt = `
+      The user got the following ${user.industry} technical interview questions wrong:
+
+      ${wrongQuestionsText}
+
+      Based on these mistakes, provide a concise, specific improvement tip.
+      Focus on the knowledge gaps revealed by these wrong answers.
+      Keep the response under 2 sentences and make it brutal.
+      Don't explicitly mention the mistakes, instead focus on what to learn/practice.
+    `;
+    try {
+      const result = await model.generateContent(improvementPrompt);
+      const response = result.response;
+      improvementTip = response.text().trim();
+    } catch (error) {
+      console.error("Error generation improvement tip", error);
+    }
+  }
+
+  try {
+    const assessment = await db.assessments.create({
+      data: {
+        userId: user.id,
+        quizScore: score,
+        questions: questionResults,
+        category: "Technical",
+        improvementTips: improvementTip,
+      },
+    });
+    return assessment;
+  } catch (error) {
+    console.error("Error saving quiz result:", error);
+    throw new Error("Failed to save quiz result");
+  }
 }
--- app/(main)/interview/mock/page.jsx
+++ app/(main)/interview/mock/page.jsx
@@ -1,7 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 import React from "react";
+import Quiz from "../_components/quiz";
 
 const MockInterviewPage = () => {
-  return <div>MockInterviewPage</div>;
+  return (
+    <div className="container mx-auto space-y-4 py-6 ">
+      <div>
+        <Link href={"/interview"}>
+          <Button variant={"link"} className={"gap-2 pl-0"}>
+            <ArrowLeft className="h-4 w-4" />
+            Back to Interview Preparation
+          </Button>
+        </Link>
+
+        <div>
+          <h1 className="text-6xl font-bold gradient-title ">Mock Interview</h1>
+          <p className="text-muted-foreground">
+            Test Your knowledge with industry-specific question
+          </p>
+        </div>
+      </div>
+
+      <Quiz />
+    </div>
+  );
 };
 
 export default MockInterviewPage;
